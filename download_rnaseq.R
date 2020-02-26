8e918d8f18a1b4bbe12547bc80aed1d5fa4e3eda
imanthonny <34114220+imanthonny@users.noreply.github.com>
2020-02-26 10:50:24 -0500

    Update download_rnaseq.R

--- download_rnaseq.R
+++ download_rnaseq.R
@@ -1,5 +1,5 @@
 library(downloader)
-basePath <- "https://orcestradata.blob.core.windows.net/gdsc/GRAY/2017/RNA-seq"
+basePath <- "https://orcestradata.blob.core.windows.net/gray/GRAY/2017/RNA-seq"
 download(file.path(basePath, "Kallisto_0.46.1.tar.gz"), destfile="/pfs/out/Kallisto_0.46.1.tar.gz")
 untar("/pfs/out/Kallisto_0.46.1.tar.gz", exdir="/pfs/out/Kallisto_0.46.1")
 file.remove("/pfs/out/Kallisto_0.46.1.tar.gz")
